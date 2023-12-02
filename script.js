9ceaeda031ef3f4faf29c260e517ef963b9930d7
Benji <97198967+BenPI88@users.noreply.github.com>
2023-12-01 19:53:44 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('container');
+
+const colors = [
+    '#000',
+    '#ff0000',
+    '#ffff00',
+    '#00ff00',
+    '#00ffff',
+    '#ff00ff',
+    '#ffffff'
+];
+
+let currentColorIndex = 0;
+
+setInterval(() => {
+    currentColorIndex++;
+    if (currentColorIndex >= colors.length) {
+        currentColorIndex = 0;
+    }
+
+    container.style.backgroundColor = colors[currentColorIndex];
+}, 500);
